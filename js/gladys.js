8eee067e9c64b365b0d1f0eeeefbb1459dc206ea
Vincent KULAK <vkulak62@gmail.com>
2021-03-21 13:01:51 +0100

    5 seconds interval

--- js/gladys.js
+++ js/gladys.js
@@ -1,4 +1,4 @@
-setInterval(detectGladysContainer, 30000);
+setInterval(detectGladysContainer, 5000);
 
 var currentHostname = window.location.hostname;
 var gladysUrl = 'http://' + currentHostname + '/signup';
