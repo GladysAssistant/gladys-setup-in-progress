e65f2842c6668da3f293e5b6589335a136205392
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-03-26 14:41:51 +0800

    Improve design of the page + JS

--- js/gladys.js
+++ js/gladys.js
@@ -1,15 +1,13 @@
-setInterval(detectGladysContainer, 5000);
-
-var currentHostname = window.location.hostname;
-var gladysUrl = 'http://' + currentHostname + '/signup';
+const currentHostname = window.location.hostname;
+const gladysUrl = "http://" + currentHostname + "/signup";
 
 function detectGladysContainer() {
-    var myRequest = new Request(gladysUrl);
-    fetch(myRequest).then(function (response) {
-        if (response.status == '200') {
-            window.location.replace(gladysUrl);
-        }
-    });
-};
+  fetch(gladysUrl).then(function (response) {
+    if (response.status == "200") {
+      window.location.replace(gladysUrl);
+    }
+  });
+}
 
 detectGladysContainer();
+setInterval(detectGladysContainer, 5000);
